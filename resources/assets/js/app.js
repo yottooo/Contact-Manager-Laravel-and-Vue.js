c2f56d8830f33ab80ac9fdfe3ba8a883f22f7c15
Yo <ednok2@abv.bg>
2019-06-18 23:00:05 +0300

    Initial commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+let axios = require('axios');
+
+Vue.component('contacts', require('./components/Contacts.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
